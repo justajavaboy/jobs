0a1eec2004b2eb2a4b9ba8735e7d0eb5b2c65218
David R. Smith <drs.code.monkey@gmail.com>
2017-07-21 13:48:35 -0700

    #149235391: production: sensitive and sealed

--- src/main/java/gov/ca/cwds/jobs/SubstituteCareProviderIndexJob.java
+++ src/main/java/gov/ca/cwds/jobs/SubstituteCareProviderIndexJob.java
@@ -1,5 +1,9 @@
 package gov.ca.cwds.jobs;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hibernate.SessionFactory;
@@ -45,6 +49,47 @@ public class SubstituteCareProviderIndexJob
   @Override
   protected boolean isDelete(ReplicatedSubstituteCareProviderR1 t) {
     return t.getReplicationOperation() == CmsReplicationOperation.D;
+  }
+
+  @Override
+  protected List<Pair<String, String>> getPartitionRanges() {
+    List<Pair<String, String>> ret = new ArrayList<>();
+
+    ret.add(Pair.of("aaaaaaaaaa", "A6I7LQP43S"));
+    ret.add(Pair.of("A6JfziT194", "B1ZHOmR4kl"));
+    ret.add(Pair.of("B1ZK0kM9bB", "CWRob2w5aA"));
+    ret.add(Pair.of("CWRsbBZ07S", "DT1Uln810S"));
+    ret.add(Pair.of("DT18kLg2dI", "EPdci4P54S"));
+    ret.add(Pair.of("EPded9HDmA", "FKdJBTUBlB"));
+    ret.add(Pair.of("FKdYk0PJnD", "GHghg6YB5h"));
+    ret.add(Pair.of("GHghK7v5wy", "HC7ouvJ33A"));
+    ret.add(Pair.of("HC7BmS8Cye", "IzkP85T62T"));
+    ret.add(Pair.of("Izk0rNyz58", "JvLGIqJ3Ku"));
+    ret.add(Pair.of("JvLHjYC3Ku", "KttkF4WKjw"));
+    ret.add(Pair.of("Kttoy651sG", "LpWX8cr197"));
+    ret.add(Pair.of("LpW5mzqHEL", "Mmf6piL4oV"));
+    ret.add(Pair.of("Mmf7x2nGAJ", "NiVfgSA2LC"));
+    ret.add(Pair.of("NiVfoFH3V3", "OeQvGxBAeK"));
+    ret.add(Pair.of("OeQvNkTBAf", "Pbljvkn30A"));
+    ret.add(Pair.of("Pblkjtzq38", "P71du5J07S"));
+    ret.add(Pair.of("P71rrFhD5x", "Q5QnUX11sG"));
+    ret.add(Pair.of("Q5QDTr58fU", "R1GecUoGjU"));
+    ret.add(Pair.of("R1Gglkp3tT", "SWELBjN41S"));
+    ret.add(Pair.of("SWE3NHfFNj", "TTEvpf29LH"));
+    ret.add(Pair.of("TTEIYWmBcL", "0CRee3d1sG"));
+    ret.add(Pair.of("0CRtfDf5W3", "1yLGjsA10S"));
+    ret.add(Pair.of("1yLK5it5ij", "2vKHMKP197"));
+    ret.add(Pair.of("2vKPbCN2qZ", "3szDtH56a7"));
+    ret.add(Pair.of("3szE3Ob03F", "4rg2m6z37S"));
+    ret.add(Pair.of("4rhjjT65vr", "5ojkIutBNz"));
+    ret.add(Pair.of("5ojPVj9Dyx", "6lmpC97FuD"));
+    ret.add(Pair.of("6lmpVGEKl8", "7iq1ikO0X1"));
+    ret.add(Pair.of("7iq4TAX43S", "8fyoWSIDAP"));
+    ret.add(Pair.of("8fyCjtkOOI", "9ceqONF197"));
+    ret.add(Pair.of("9cevdVN1MG", "999WyW137S"));
+    ret.add(Pair.of("999XWkeLt8", "9999999999"));
+
+    return ret;
   }
 
   /**
