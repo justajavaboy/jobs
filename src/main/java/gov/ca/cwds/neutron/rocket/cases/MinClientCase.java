167c2914b87e89f0ec375f47e1f4fa0e998abee3
David R. Smith <David.Smith@osi.ca.gov>
2017-12-14 14:55:51 -0800

    JIRA INT-532: missing cases

---
+++ src/main/java/gov/ca/cwds/neutron/rocket/cases/MinClientCase.java
@@ -0,0 +1,45 @@
+package gov.ca.cwds.neutron.rocket.cases;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import gov.ca.cwds.data.std.ApiMarker;
+
+/**
+ * Convenient carrier bean for client/case keys.
+ * 
+ * @author CWDS API Team
+ */
+public class MinClientCase implements ApiMarker {
+
+  private static final long serialVersionUID = 1L;
+
+  private String clientId;
+  private String caseId;
+
+  public MinClientCase(String clientId, String caseId) {
+    this.clientId = clientId;
+    this.caseId = caseId;
+  }
+
+  public static MinClientCase extract(final ResultSet rs) throws SQLException {
+    return new MinClientCase(rs.getString("CLIENT_ID"), rs.getString("CASE_ID"));
+  }
+
+  public String getClientId() {
+    return clientId;
+  }
+
+  public void setClientId(String clientId) {
+    this.clientId = clientId;
+  }
+
+  public String getCaseId() {
+    return caseId;
+  }
+
+  public void setCaseId(String referralId) {
+    this.caseId = referralId;
+  }
+
+}
