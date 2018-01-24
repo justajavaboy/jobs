9148ac5d7cfaaa8c0c6a7805924caedd47d6b903
David R. Smith <David.Smith@osi.ca.gov>
2018-01-24 14:19:24 -0800

    INT-1051: DB2 schema refresh

--- src/main/java/gov/ca/cwds/neutron/rocket/SchemaResetRocket.java
+++ src/main/java/gov/ca/cwds/neutron/rocket/SchemaResetRocket.java
@@ -58,7 +58,6 @@ public class SchemaResetRocket
       JobLogs.checked(LOGGER, e, "SCHEMA REFRESH ERROR!! {}", e.getMessage());
     }
 
-    LOGGER.warn("SCHEMA REFRESH KICKED OFF!!!");
     return lastRunDate;
   }
 
@@ -73,11 +72,12 @@ public class SchemaResetRocket
       final Session session = getJobDao().getSessionFactory().getCurrentSession();
       getOrCreateTransaction(); // HACK
 
-      // NS schema, not RS.
-      final String targetSchema = "CWSNS4"; // TESTING ONLY!!
-      // (String) session.getSessionFactory().getProperties().get("hibernate.default_schema");
+      // Target the NS schema, not RS.
+      final String targetSchema =
+          ((String) session.getSessionFactory().getProperties().get("hibernate.default_schema"))
+              .replaceFirst("CWSRS", "CWSNS").replaceAll("\"", "");
 
-      // TODO: For now the proc lives only in NS1 but affects the target schema.
+      // HACK: The proc currently lives in NS1 but affects the target schema.
       final ProcedureCall proc = session.createStoredProcedureCall("CWSNS1.SPREFRSNS1");
       proc.registerStoredProcedureParameter("SCHEMANM", String.class, ParameterMode.IN);
       proc.registerStoredProcedureParameter("RETSTATUS", String.class, ParameterMode.OUT);
@@ -92,6 +92,8 @@ public class SchemaResetRocket
 
       if (returnStatus.charAt(0) != '0') {
         JobLogs.runtime(LOGGER, "SCHEMA REFRESH ERROR! {}", returnMsg);
+      } else {
+        LOGGER.warn("SCHEMA REFRESH KICKED OFF!!!");
       }
     } else {
       LOGGER.warn("SAFETY! REFRESH PROHIBITED ON LARGE DATA SETS!");
