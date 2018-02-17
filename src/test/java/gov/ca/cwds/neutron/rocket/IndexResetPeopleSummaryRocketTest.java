53d3c17666f037e8279156dcdd98db4fd4ed06df
David R. Smith <David.Smith@osi.ca.gov>
2018-02-17 08:40:28 -0800

    SonarQube, Javadoc

---
+++ src/test/java/gov/ca/cwds/neutron/rocket/IndexResetPeopleSummaryRocketTest.java
@@ -0,0 +1,52 @@
+package gov.ca.cwds.neutron.rocket;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+import gov.ca.cwds.dao.cms.ReplicatedOtherAdultInPlacemtHomeDao;
+import gov.ca.cwds.data.persistence.cms.rep.ReplicatedOtherAdultInPlacemtHome;
+import gov.ca.cwds.jobs.Goddard;
+
+public class IndexResetPeopleSummaryRocketTest
+    extends Goddard<ReplicatedOtherAdultInPlacemtHome, ReplicatedOtherAdultInPlacemtHome> {
+
+  ReplicatedOtherAdultInPlacemtHomeDao dao;
+  IndexResetPeopleSummaryRocket target;
+
+  @Override
+  public void setup() throws Exception {
+    super.setup();
+
+    dao = new ReplicatedOtherAdultInPlacemtHomeDao(sessionFactory);
+    target = new IndexResetPeopleSummaryRocket(dao, esDao, mapper, flightPlan);
+  }
+
+  @Test
+  public void type() throws Exception {
+    assertThat(IndexResetPeopleSummaryRocket.class, notNullValue());
+  }
+
+  @Test
+  public void instantiation() throws Exception {
+    assertThat(target, notNullValue());
+  }
+
+  @Test
+  public void getIndexSettingsLocation_A$() throws Exception {
+    String actual = target.getIndexSettingsLocation();
+    String expected = null;
+    assertThat(actual, is(equalTo(expected)));
+  }
+
+  @Test
+  public void getDocumentMappingLocation_A$() throws Exception {
+    String actual = target.getDocumentMappingLocation();
+    String expected = null;
+    assertThat(actual, is(equalTo(expected)));
+  }
+
+}
