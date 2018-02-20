d7a00f7956381be90b3aca6bff66282fa4f535f4
David R. Smith <David.Smith@osi.ca.gov>
2018-02-20 07:42:49 -0800

    SonarQube, Javadoc

--- src/test/java/gov/ca/cwds/neutron/vox/jmx/VoxCommandFactoryTest.java
+++ src/test/java/gov/ca/cwds/neutron/vox/jmx/VoxCommandFactoryTest.java
@@ -4,107 +4,50 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
-import static org.mockito.Mockito.mock;
 
 import org.junit.Test;
 
 import gov.ca.cwds.jobs.Goddard;
 import gov.ca.cwds.neutron.exception.NeutronCheckedException;
 import gov.ca.cwds.neutron.vox.VoxCommandInstruction;
+import gov.ca.cwds.neutron.vox.jmx.cmd.VoxCommandLastRunStatus;
 
 public class VoxCommandFactoryTest extends Goddard {
 
   @Test
   public void type() throws Exception {
-
     assertThat(VoxCommandFactory.class, notNullValue());
   }
 
   @Test
   public void build_A$VoxCommandType$VoxCommandInstruction() throws Exception {
-
-
-    VoxCommandType cmdType = mock(VoxCommandType.class);
-    VoxCommandInstruction cmd = mock(VoxCommandInstruction.class);
-
-
-    VoxJMXCommandClient actual = VoxCommandFactory.build(cmdType, cmd);
-
-
-    VoxJMXCommandClient expected = null;
+    final VoxCommandType cmdType = VoxCommandType.DISABLE;
+    final String[] args = new String[] {};
+    VoxCommandInstruction cmd = VoxCommandInstruction.parseCommandLine(args);
+    final VoxJMXCommandClient actual = VoxCommandFactory.build(cmdType, cmd);
+    final VoxJMXCommandClient expected = new VoxCommandLastRunStatus();
     assertThat(actual, is(equalTo(expected)));
   }
 
-  @Test
+  @Test(expected = NeutronCheckedException.class)
   public void build_A$VoxCommandType$VoxCommandInstruction_T$NeutronCheckedException()
       throws Exception {
-
-
-    VoxCommandType cmdType = mock(VoxCommandType.class);
-    VoxCommandInstruction cmd = mock(VoxCommandInstruction.class);
-
-    try {
-
-      VoxCommandFactory.build(cmdType, cmd);
-      fail("Expected exception was not thrown!");
-    } catch (NeutronCheckedException e) {
-
-    }
+    final VoxCommandType cmdType = null;
+    final String[] args = new String[] {"-r", "fake_rocket", "-c", "fake_cmd"};
+    VoxCommandInstruction cmd = VoxCommandInstruction.parseCommandLine(args);
+    VoxCommandFactory.build(cmdType, cmd);
   }
 
-  @Test
-  public void launch_A$StringArray() throws Exception {
-
-
-    String[] args = new String[] {};
-
-
-    VoxCommandFactory.launch(args);
-
-
-  }
-
-  @Test
+  @Test(expected = NeutronCheckedException.class)
   public void launch_A$StringArray_T$NeutronCheckedException() throws Exception {
-
-
-    String[] args = new String[] {};
-
-    try {
-
-      VoxCommandFactory.launch(args);
-      fail("Expected exception was not thrown!");
-    } catch (NeutronCheckedException e) {
-
-    }
+    final String[] args = new String[] {};
+    VoxCommandFactory.launch(args);
   }
 
-  @Test
-  public void main_A$StringArray() throws Exception {
-
-
-    String[] args = new String[] {};
-
-
-    VoxCommandFactory.main(args);
-
-
-  }
-
-  @Test
+  @Test(expected = Exception.class)
   public void main_A$StringArray_T$Exception() throws Exception {
-
-
-    String[] args = new String[] {};
-
-    try {
-
-      VoxCommandFactory.main(args);
-      fail("Expected exception was not thrown!");
-    } catch (Exception e) {
-
-    }
+    final String[] args = new String[] {};
+    VoxCommandFactory.main(args);
   }
 
 }
